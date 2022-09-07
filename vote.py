56be5ab658b1076b764eeadf65ffbc6b07b181cb
Dhanalakshmi192120016 <112929640+Dhanalakshmi192120016@users.noreply.github.com>
2022-09-07 14:16:48 +0530

    Add files via upload

---
+++ vote.py
@@ -0,0 +1,14 @@
+#Vote
+try:
+    age=int(input("Enter your age: "))
+    if (age>=18):
+        print("You can vote")
+    elif(0<=age<18):
+        print("You are allowed to vote after",18-age,"years")
+    else:
+        print("Age cannot be negative")
+
+except ValueError:
+        print("Enter a valid whole number")
+    
+    
