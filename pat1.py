e82fa4f3acf34f52d825b31870eaa3056eb8d3da
Dhanalakshmi192120016 <112929640+Dhanalakshmi192120016@users.noreply.github.com>
2022-09-08 10:43:13 +0530

    Add files via upload

---
+++ pat1.py
@@ -0,0 +1,6 @@
+#pattern_1
+row=int(input("Enter the number of rows: "))
+for i in range(1,row+1):
+    for a in range(i):
+        print(i,end='')
+    print()
--- perfect_or_not.py
+++ perfect_or_not.py
@@ -1,14 +1,14 @@
 try:
-    num = int(input(" Please Enter any Number: "))
+    num = int(input(" Enter the number: "))
     Sum = 0
     if(num>=0):
         for i in range(1, num,1):
             if(num % i == 0):
                 Sum = Sum + i
         if (Sum == num):
-            print(" %d is a Perfect Number" %num)
+            print(num, "is a Perfect Number")
         else:
-            print(" %d is not a Perfect Number" %num)
+            print(num,"is not a Perfect Number")
     else:
         print("Enter a positive integer")
         
