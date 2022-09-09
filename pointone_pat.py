55520a2dba7c12688ff06c5feca3e3adc7118983
Dhanalakshmi192120016 <112929640+Dhanalakshmi192120016@users.noreply.github.com>
2022-09-09 12:02:57 +0530

    Add files via upload

---
+++ pointone_pat.py
@@ -0,0 +1,7 @@
+#0.1_pattern
+rows = int(input("number of rows: "))
+for i in range(1,rows+1):
+    for j in range (1,i+1):
+        print(j/10,end=" ")
+    print("\r")
+    
---
+++ tri_star_pat.py
@@ -0,0 +1,10 @@
+#triangle_star
+r=int(input("Enter the number of rows: "))
+k=(2*r)-2
+for i in range(0,r):
+    for j in range(0,k):
+        print(end=" ")
+    k=k-1
+    for j in range(0,i+1):
+        print("*",end=" ")
+    print("\r")
